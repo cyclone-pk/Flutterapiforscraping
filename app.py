e58df790eb439562aff7cc504d080e0861f23fd2
UNKNOWN <toptutorial>
2019-08-18 16:57:29 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask , request , jsonify
+from bs4 import BeautifulSoup
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/',methods=['GET'])
+def API():
+    if request.method == 'GET':
+        uri = 'https://www.brainyquote.com'
+        query = str(request.args['query'])
+        print(query)
+        if " " in query:
+            query = str(query).replace(" ","+")
+        else:
+            pass
+
+        search = '/search_results?q=' + query
+
+        ready_uri = uri + search
+        print(ready_uri)
+        content = requests.get(ready_uri).content
+        soup = BeautifulSoup(content, 'html.parser')
+        quotes_links = soup.find_all('a', {'class': 'b-qt'})
+        l = []
+        for i in quotes_links:
+            d = {}
+            quote_url = uri + i.get('href')
+            quote_content = requests.get(quote_url).content
+            quote_soup = BeautifulSoup(quote_content, 'html.parser')
+            d['quote'] = quote_soup.find('p', {'class': 'b-qt'}).text
+            d['author'] = quote_soup.find('p', {'class': 'bq_fq_a'}).text
+            l.append(d)
+
+
+        return jsonify(l)
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
+
+
