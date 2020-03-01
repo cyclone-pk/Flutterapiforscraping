5b79716ed962e707c344b57a0d43526415ee097a
cyclone-pk <49232158+cyclone-pk@users.noreply.github.com>
2020-03-01 11:11:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 app = Flask(__name__)
 
 
-@app.route('/',methods=['GET'])
+@app.route('/api/v1/',methods=['GET'])
 def API():
     if request.method == 'GET':
         uri = 'https://www.brainyquote.com'
